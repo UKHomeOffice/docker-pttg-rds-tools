334851d39339838b99c70719acdcca9eb4b6ef0b
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-09-16 12:34:00 +0100

    EE-12556 Add function to stop RDS instance

---
+++ scripts/stop-rds.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function stopRdsInstance() {
+    for attempt in {1..10}
+    do
+        stopping_status=$(aws rds stop-db-instance --db-instance-identifier ${RDS_INSTANCE} | jq -r '.[].DBInstanceStatus')
+        if [[ ${stopping_status} == "stopping" ]]; then
+            break
+        fi
+        sleep 1
+    done
+    echo ${stopping_status}
+}
+
+stopRdsInstance
