4ef072da26b9f0a9862558a5e47fc370bf6a9d48
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-09-16 15:39:57 +0100

    EE-12559 Listen to STOP_RDS env var

--- scripts/stop-rds.sh
+++ scripts/stop-rds.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 function stopRdsInstance() {
+    if [[ "$STOP_RDS" != "true" ]]; then
+        echo "Not stopping RDS instance - STOP_RDS=${STOP_RDS}"
+    fi
+
     for attempt in {1..10}
     do
         stopping_status=$(aws rds stop-db-instance --db-instance-identifier ${RDS_INSTANCE} | jq -r '.[].DBInstanceStatus')
