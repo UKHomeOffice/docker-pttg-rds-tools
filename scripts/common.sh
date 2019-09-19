8607c4038066e898b447e0b7c7a2d1a0c1843d6a
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-09-19 13:48:59 +0100

    EE-12559 Address review comments

--- scripts/common.sh
+++ scripts/common.sh
@@ -19,14 +19,14 @@ function waitForInstanceAvailable() {
     for attempt in {1..10}
     do
         current_status=$(getRdsStatus)
-        if [[ "$current_status" == "available" ]]; then
+        if [[ "${current_status}" == "available" ]]; then
             echo "${RDS_INSTANCE} is now available"
             break
-        elif [[ "$current_status" != "starting" ]] && [[ "$current_status" != "rebooting" ]]; then
-            echo "${RDS_INSTANCE} is at status $current_status. Aborting as this will not become available."
-            break
-        else
+        elif [[ "${current_status}" == "starting" ]] || [[ "${current_status}" == "rebooting" ]]; then
             echo  "${RDS_INSTANCE} is still starting. Waiting until available."
+        else
+            echo "${RDS_INSTANCE} is at status ${current_status}. Aborting as this will not become available."
+            break
         fi
 
         sleep ${STATUS_WAIT_TIME_SECONDS}
@@ -47,7 +47,7 @@ function startRdsInstance() {
 
 function startRdsInstanceIfStopped() {
     instance_status=$(getRdsStatus)
-    if [[ "$instance_status" == "stopped" ]]; then
+    if [[ "${instance_status}" == "stopped" ]]; then
         echo "RDS instance status = $instance_status.  Starting RDS instance ${RDS_INSTANCE}"
         start_status=$(startRdsInstance)
         if [[ ${start_status} == "starting" ]]; then
@@ -56,7 +56,7 @@ function startRdsInstanceIfStopped() {
             echo "Failed to start RDS instance ${RDS_INSTANCE}"
         fi
     else
-        echo "RDS instance ${RDS_INSTANCE} is in state $instance_status - cannot start instance."
+        echo "RDS instance ${RDS_INSTANCE} is in state ${instance_status} - cannot start instance."
     fi
 }
 
